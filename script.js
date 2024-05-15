fc1ca72f585d28e2e706d393221619c6c6117896
kakuwawawa <168386466+kakuwawawa@users.noreply.github.com>
2024-05-15 23:56:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const buttonContainer = document.getElementById('buttonContainer');
 
 const names = ['加藤 涼太', '堀田 悠人', '前田 和也', '菊池 良翼', '増田 啓太', '増田 遥斗', '栗下 優大', '松本 侑大', '三浦 壮太', '榊原 拓真', '三澤 遥斗', '武藤 倫', '榊原 丈了', '宮崎 正寛', '元木 悠人', '指方 拓仁', '亀田 蒼空', '森田 大地', '今泉 虹希', '足立 翔哉', '矢治 優輝', '田上 大斗', '津崎 壮達', '山内 涼生', '守田 拓未', '古屋 凱斗', '山口 侑貴', '田副 澄海登', '西村 柾哉', '山崎 天翔', '山田 瑠生', '山本 一磨','脇野 羚央' ,'鷲見 侑飛'];
-
 const statusBtns = [];
 let isAtHome = JSON.parse(localStorage.getItem('isAtHome')) || Array.from({ length: 34 }, () => false);
 
